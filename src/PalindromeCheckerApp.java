81f055ca83ec1e3300921b464c37a72eb9fa2add
Kirushma21 <bpkirushma21@gmail.com>
2026-04-07 09:40:29 +0530

    UC1

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,17 @@
+public class PalindromeCheckerApp {
+    public static void main() {
+        System.out.println("=======================================");
+        System.out.println("     Palindrome Checker Application    ");
+        System.out.println("=======================================");
+
+
+        System.out.println("Version: 1.0");
+
+
+        System.out.println("Welcome! This app checks whether a given string is a palindrome.");
+
+        System.out.println("Proceeding to next use case...");
+
+
+    }
+}
